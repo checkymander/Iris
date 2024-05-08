e677174f3fd60441aaccb907c1800e9c6620d040
checkymander <checkymander@protonmail.com>
2024-05-08 15:39:41 -0400

    add debug output

--- Payload_Type/iris/iris/mythic/agent_functions/helpers/tools/MythicRPCSpec.py
+++ Payload_Type/iris/iris/mythic/agent_functions/helpers/tools/MythicRPCSpec.py
@@ -17,43 +17,43 @@ class MythicRPCSpec(BaseToolSpec):
         """Finds a specific callback by its agent_callback_id (UUID)"""
 
         id = await self._check_valid_id(agent_callback_id)
-        self._debug_print(f"Checking for callback id: {id}")
+        self._debug_print("get_callback_by_uuid_async", f"Checking for callback id: {id}")
         search_message = MythicRPCCallbackSearchMessage(AgentCallbackUUID=self._scope,
                                                         SearchCallbackUUID=id)
         response = await SendMythicRPCCallbackSearch(search_message)
 
         if response.Success:
-            self._debug_print(f"Found callback!")
+            self._debug_print("get_callback_by_uuid_async", f"Found callback!")
             return response.Results[0].to_json()
         else:
-            self._debug_print(f"Callback not found!")
+            self._debug_print("get_callback_by_uuid_async", f"Callback not found!")
             return json.dumps({"message":"Callback Not Found"})
     
     async def task_callback(self, agent_callback_id:str, command:str, params: str):
         """Executes a command on a callback specified by its agent_callback_id with parameters specified by a json string of parameter names and parameter values"""
         id = await self._check_valid_id(agent_callback_id)
-        self._debug_print(f"Executing command: {command} with params {params} on callback {id}")
+        self._debug_print("task_callback", f"Executing command: {command} with params {params} on callback {id}")
         response = await SendMythicRPCTaskCreate(MythicRPCTaskCreateMessage(AgentCallbackID=id, CommandName=command, Params=params))
 
         if response.Success:
-            self._debug_print(f"Task Started with ID: {response.TaskID}")
+            self._debug_print("task_callback", f"Task Started with ID: {response.TaskID}")
             return f"Task {response.TaskID} started."
         else:
-            self._debug_print(f"Failed to issue task: {response.Error}")
+            self._debug_print("task_callback", f"Failed to issue task: {response.Error}")
             return f"Failed to issue task: {response.Error}"
         
     async def map_callback_number_to_agent_callback_id(self, callback: int):  
         """Converts a numeric callback ID to an Agent Callback UUID"""
-        self._debug_print(f"Checking for callback id: {callback}")
-        self._debug_print(f"Agent scope: {self._scope}")
+        self._debug_print("map_callback_number_to_agent_callback_id", f"Checking for callback id: {callback}")
+        self._debug_print("map_callback_number_to_agent_callback_id", f"Agent scope: {self._scope}")
         search_message = MythicRPCCallbackSearchMessage(AgentCallbackUUID=self._scope,
                                                         SearchCallbackDisplayID=callback)
         response = await SendMythicRPCCallbackSearch(search_message)
         if response.Success:
-            self._debug_print(f"Success! {len(response.Results)}")
+            self._debug_print("map_callback_number_to_agent_callback_id", f"Success! {len(response.Results)}")
             return response.Results[0].AgentCallbackID
         else:
-            self._debug_print(f"No Callback Found")
+            self._debug_print("map_callback_number_to_agent_callback_id", f"No Callback Found")
             return "Agent ID not found"
     
     def _is_valid_uuid(self, val):
@@ -63,27 +63,27 @@ class MythicRPCSpec(BaseToolSpec):
         except ValueError:
             return False
         
-    def _debug_print(self, message):
+    def _debug_print(self, function, message):
         if self._debug:
-            print(message)
+            print(f"[{function} {message}]")
     
     async def _check_valid_id(self, val) -> str:
-        self._debug_print(f"Checking if already a UUID")
+        self._debug_print("_check_valid_id", f"Checking if already a UUID")
         if self._is_valid_uuid(val):
-            self._debug_print(f"Value is already a UUID")
+            self._debug_print("_check_valid_id", f"Value is already a UUID")
             return val
         else:
             try:
-                self._debug_print(f"Value is a display callback")
+                self._debug_print("_check_valid_id", f"Value is a display callback")
                 id = await self.map_callback_number_to_agent_callback_id(callback=val)
-                self._debug_print(f"Value returned is: {id}")
+                self._debug_print("_check_valid_id", f"Value returned is: {id}")
                 if self._is_valid_uuid(id):
-                    self._debug_print(f"Found valid agent UUID for callback {val} - UUID: {id}")
+                    self._debug_print("_check_valid_id", f"Found valid agent UUID for callback {val} - UUID: {id}")
                     return id
             except:
-                self._debug_print(f"No valid UUID found")
+                self._debug_print("_check_valid_id", f"No valid UUID found")
                 return ""
-        self._debug_print(f"No valid UUID found")
+        self._debug_print("_check_valid_id", f"No valid UUID found")
         return ""
 
     async def get_dangerous_processes(self, Host:str):
@@ -106,12 +106,12 @@ class MythicRPCSpec(BaseToolSpec):
                 id = response.Files[0].AgentFileId
             else:
                 return "File Not Found"
-        self._debug_print(f"Getting file contents for ID: {id}")
+        self._debug_print("get_file_contents", f"Getting file contents for ID: {id}")
         file_response = await SendMythicRPCFileGetContent(MythicRPCFileGetContentMessage(AgentFileId=id))
 
         if file_response.Success:
-            self._debug_print(f"Successfully found file of length: {len(base64.b64decode(file_response.Content))}")
+            self._debug_print("get_file_contents", f"Successfully found file of length: {len(base64.b64decode(file_response.Content))}")
             return base64.b64decode(file_response.Content)
         else:
-            self._debug_print(f"File Not Found")
+            self._debug_print("get_file_contents", f"File Not Found")
             return "File Not Found"
