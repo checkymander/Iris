264591fe72ce1860b4fd7f55361b536f390c394e
checkymander <scottie.austin1991@gmail.com>
2024-12-11 15:59:12 -0500

    async is hard

--- Payload_Type/iris/iris/mythic/agent_functions/tools/CallbackTools.py
+++ Payload_Type/iris/iris/mythic/agent_functions/tools/CallbackTools.py
@@ -2,7 +2,8 @@ import asyncio
 import nest_asyncio
 from mythic_container.MythicCommandBase import *
 from mythic_container.MythicRPC import *
-from ..helpers.async_helper import *
+from iris_utils.async_helper import *
+
 def get_callback_by_uuid(agent_callback_id: str) -> str:
     return run_async_function(get_callback_by_uuid_async, agent_callback_id)
 
