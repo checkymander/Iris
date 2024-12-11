cd0e2ff8b5c4bfdbe48bd278df2d0c2269bb2f21
checkymander <scottie.austin1991@gmail.com>
2024-12-11 15:16:25 -0500

    async is hard

--- Payload_Type/iris/iris/mythic/agent_functions/tools/CallbackTools.py
+++ Payload_Type/iris/iris/mythic/agent_functions/tools/CallbackTools.py
@@ -3,9 +3,9 @@ from mythic_container.MythicCommandBase import *
 from mythic_container.MythicRPC import *
 
 def get_callback_by_uuid(agent_callback_id: str) -> str:
-    loop = asyncio.get_running_loop()
-    result = loop.create_task(get_callback_by_uuid_async(agent_callback_id))
-    return result.result
+    #loop = asyncio.get_running_loop()
+    return asyncio.create_task(get_callback_by_uuid_async(agent_callback_id)).result()
+    #return result.result()
 
 async def get_callback_by_uuid_async(agent_callback_id: str) -> str:
     print(f"\nAgent Callback ID: {agent_callback_id}")
